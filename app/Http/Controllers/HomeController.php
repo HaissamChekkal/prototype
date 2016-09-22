c0db90d2372d197843527a25d966f874efa0741e
timothee <tim.dorand@gmail.com>
2016-09-22 12:24:12 +0200

    Affichage des projets réalisés

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $events = Event::all();
+        return view('event.index')->with(compact('events'));
     }
 }
