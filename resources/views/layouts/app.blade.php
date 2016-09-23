8142b8f9a7b05751cb971e2cf9e2da5e262a6dc4
Baptiste BRIOIS <baptistebriois@MacBook-Pro-de-Baptiste.local>
2016-09-23 12:32:36 +0200

    fix name app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'PlaceIt') }}</title>
+    <title>{{ config('app.name', 'Stick & Move') }}</title>
 
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
@@ -36,7 +36,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'PlaceIt') }}
+                    {{ config('app.name', 'Stick & Move') }}
                 </a>
             </div>
 
